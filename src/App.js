e5ec09a32ba8cc891ec926bdea0f9317e25e06bb
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-11-01 02:00:27 +0545

    props down to post.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,13 @@ function App() {
       </div>
 
       {/* Posts */}
+      <Post
+        username="codewithsparsh"
+        caption="Let's get to work!!"
+        imageUrl="https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
+      />
+      <Post />
+      <Post />
       <Post />
     </div>
   );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,7 +2,7 @@ import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import "./Post.css";
 
-const Post = () => {
+const Post = ({ username, caption, imageUrl }) => {
   return (
     <div className="post">
       {/* Avatar + Username */}
@@ -12,18 +12,16 @@ const Post = () => {
           alt="Sparsh Sukralia"
           src="/images/1.png"
         />
-        <h3>Username</h3>
+        <h3>{username}</h3>
       </div>
 
       {/* image */}
-      <img
-        className="post__image"
-        src="https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
-      />
+      <img className="post__image" src={imageUrl} />
 
       {/* username + caption */}
       <h4 className="post__text">
-        <strong>codewithsparh</strong> Let's get to work
+        <strong>{username}</strong>
+        {caption}
       </h4>
     </div>
   );
