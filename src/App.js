14e0947289a1bbcd12de61dcc71a236ea3cba179
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-11-01 02:48:09 +0545

    mapping on posts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Post from "./components/Post/Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "codewithsparsh",
+      caption: "Let's get to work!!",
+      imageUrl:
+        "https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60",
+    },
+    {
+      username: "sparshsukralia",
+      caption: "It's a cute pup!!",
+      imageUrl:
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*",
+    },
+  ]);
+
   return (
     <div className="app">
       <div className="app__header">
@@ -13,14 +28,16 @@ function App() {
       </div>
 
       {/* Posts */}
-      <Post
-        username="codewithsparsh"
-        caption="Let's get to work!!"
-        imageUrl="https://images.unsplash.com/photo-1517404215738-15263e9f9178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60"
-      />
-      <Post />
-      <Post />
-      <Post />
+
+      {posts.map((post) => {
+        return (
+          <Post
+            username={post.username}
+            caption={post.caption}
+            imageUrl={post.imageUrl}
+          />
+        );
+      })}
     </div>
   );
 }
