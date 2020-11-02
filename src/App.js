4355b094153ed588bd4e21ce887fa28275008cba
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-11-02 17:59:06 +0545

    material-ui modal

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import { db } from "./firebase";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
 
   // [] means the code runs once when the page loads and not again
   // [posts] means the code runs when the page loads and also whenever the content of posts changes
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
       // everytime a new post is added this piece of code fires off
-      setPosts(snapshot.docs.map((doc) => doc.data()));
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
     });
   }, [posts]);
 
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -26,9 +38,10 @@ function App() {
 
       {/* Posts */}
 
-      {posts.map((post) => {
+      {posts.map(({ id, post }) => {
         return (
           <Post
+            key={id}
             username={post.username}
             caption={post.caption}
             imageUrl={post.imageUrl}
