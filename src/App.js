f4646b0c97c50805b2521acd359cd1411d23449c
Sparsh Sukralia <sparsh.sukralia@gmail.com>
2020-11-02 18:26:18 +0545

    modal imports

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./components/Post/Post";
 import { db } from "./firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const classes = useStyles();
+
+  const [modalStyle] = useState(getModalStyle);
 
   // [] means the code runs once when the page loads and not again
   // [posts] means the code runs when the page loads and also whenever the content of posts changes
